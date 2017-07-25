d43dea38ea176053e59d2779a839fb46e217b9ec
Nicholas H <nhung93@outlook.com>
2017-07-24 23:54:29 -0400

    got get and delete to work with mongodb for ascii faces! ^^

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # funTalk
-a basic chat application using Node.js, Socket.IO 
-    
-Initially I thought of using Node.js with PHP and MySQL, but I think now I better understand the purpose of these technologies and realize that using PHP here probably doesn't make much sense. All I really need to do is set up a server and a database. Since Node.js can already set up a server and can be used to talk to databases, I think that's all I need. I just need to set up my database. I think I might try out MongoDB for this, but MySQL is fun too (a little painful to set up though).    
-    
+a basic chat application using Node.js, Socket.IO, MongoDB    
+      
 The really cool feature that I think will make my application different from any other chat application out there is the fact that users will be able to preset emotions (i.e. happy, angry, sad) mapped to a variety of pictures and ASCII emoticons. That way when you're chatting and just words won't do, you don't have to search online for a particular ASCII emoticon or an anime reaction face to suit your needs. 
      
-Ideally, users should be able to login with a username and password and customize their presets, which includes the ability to import new pictures. Therefore, maintaining a simple database will be necessary.
+Ideally, users should be able to login with a username and password and customize their presets, which includes the ability to import new pictures. Therefore, maintaining a simple database will be necessary.    
+
--- index.js
+++ index.js
@@ -1,15 +1,127 @@
+// set up server 
+// also hooking up database (mongodb) 
 
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// database stuff here !
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+
+// this will serve the login page to the user first!
+// if login is successful, then the server can serve the chat page
 app.get('/', function(req, res){
-	//res.send('<h1> Hello </h1>');
+	
+	//res.sendFile(__dirname + '/login.html');
+	
+	// this says: "send the html page to the client"
 	res.sendFile(__dirname + '/index.html');
+});
+
+// this is for when a login is successful. send the client the chat page.
+app.get('/test', function(req, res){
+	console.log("i see the request.");
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+// when user loads page, client page should automatically send a getJSON request to this server
+// the request will ask for a url, which the below will respond to. simply connect to the database
+// and send back the default ascii faces info as the response. 
+app.get('/default', function(req, res){
+	
+	// connect with database 
+	var url = 'mongodb://127.0.0.1:27017';
+	MongoClient.connect(url, function(err, db){
+		assert.equal(null, err);
+		console.log("getting the default ascii faces ^_^...");
+		
+		// get the default emoticons 
+		var defaultSet = db.collection('users').findOne({"_id": "default"}, function(err, result){
+			res.send(result); // send back to client the associative array with all the default emoticons 
+		});
+		
+		db.close();
+	});
+});
+
+
+// this is for when the user deletes an ascii face 
+// btw, an interesting thing about using variable names for keys in objects:
+// https://stackoverflow.com/questions/2274242/using-a-variable-for-a-key-in-a-javascript-object-literal
+// https://stackoverflow.com/questions/17039018/how-to-use-a-variable-as-a-field-name-in-mongodb-native-findone
+app.delete('/delete_asciiface', function(req, res){
+	
+	// get the queries' values so we know which ascii face user wants to delete 
+	var faceCategory = req.query.category;
+	var selectedFace = req.query.face;
+	
+	// connect with db 
+	var url = 'mongodb://127.0.0.1:27017';
+	MongoClient.connect(url, function(err, db){
+		assert.equal(null, err);
+		console.log("removing the selected emoticon...");
+		
+		// do delete here 
+		var categoryToFind = "ascii_" + faceCategory;
+		var query = {};
+		query[categoryToFind] = { $in: [selectedFace] };
+		var removeAscii = {$pull: ""};
+		
+		// can't acess $pull operator directly, so try loop 
+		for(prop in removeAscii){
+			removeAscii[prop] = query;
+		}
+		
+		// element should be removed here after update 
+		db.collection('users').update({"_id": "default"}, removeAscii);
+		
+		// then send back to client the updated info 
+		var updatedSet = db.collection('users').findOne({"_id": "default"}, function(err, result){
+			//console.log(result);
+			res.send(result); 
+		});
+		
+		db.close();
+	});
 });
 
 io.on('connection', function(socket){
 	console.log('a user connected');
+
+	// this is where the database lives 
+	var url = 'mongodb://127.0.0.1:27017/admin';
+	
+	// when server is on, the database will also be connected  
+	MongoClient.connect(url, function(err, db){
+		assert.equal(null, err);
+		console.log("connected to server");
+		
+		// create (or use if already existing) new collection called 'users' 
+		var coll = db.collection('users');
+		
+		// this is the collection of default ascii faces
+		// the upsert should prevent adding duplicates 
+		coll.update(
+			{"_id": "default"},
+			// this object represents the default user! so id is default. 
+			{   
+				"_id": "default",  
+				"ascii_happy": ["^_^", ":)", ":D"],
+				"ascii_sad": [":<"], 
+				"ascii_angry": ["（　ﾟДﾟ）", ">:|"], 
+				"ascii_funny": ["¯\\_(ツ)_/¯"],
+				"ascii_other": ["ʕ•ᴥ•ʔ"]
+			},
+			{upsert: true}
+		);
+		
+		db.close();
+	});
+
+	
 	socket.on('chat message', function(msg){
 		// this is the server sending out the message to every client
 		
@@ -21,6 +133,7 @@ io.on('connection', function(socket){
 		// https://stackoverflow.com/questions/12882885/how-to-add-nbsp-using-d3-js-selection-text-method
 		io.emit('chat message', msg + "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 " + timestamp);
 	});
+	
 	socket.on('image', function(imgData){
 		// send all clients the imgData that was sent here (to this server)
 		io.emit('image', imgData);
